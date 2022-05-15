ffdb753ba5c3c77804077ebd07083a2d2a96d9b3
hardipgajera <gajerahardip789@gmail.com>
2022-05-15 23:31:32 +0530

    Initial Commit

---
+++ CloudNotes/CloudNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudNotesApp.swift
+//  CloudNotes
+//
+//  Created by hardip gajera on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
